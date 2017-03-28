b061d3472edc466a2f87497979018c68706c8286
Abhishek Khandait <khandaitabhishek@gmail.com>
2017-03-28 21:21:12 +0530

    created home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { _tenants } from '../models/tenant.model';
+
+@Component({
+  selector: 'es-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  tenants = _tenants;
+  constructr() { }
+
+  ngOnInit() {
+  }
+
+}
