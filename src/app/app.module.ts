46f6a384ecb1906fdf2d546b3a4bd8a6f6e5ae46
Abhishek Khandait <khandaitabhishek@gmail.com>
2017-03-28 21:35:16 +0530

    * added home and dashboard compoents * configured routing for newly added components * removed @ng-bootsrap/ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+//import { AppRoutingModule } from './app.router';
 import { NavComponent } from './core/nav/nav.component';
+
+import { HomeComponent } from './home/home.component';
+import { CardComponent } from './home/shared/card/card.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+//import { Tenant } from './models/tenant.model'
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent, },
+    { path: 'dashboard', component: DashboardComponent, }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    //AppRoutingModule
+    HomeComponent,
+    CardComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     FormsModule,
-    HttpModule,
-    NgbModule.forRoot()
+    HttpModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
