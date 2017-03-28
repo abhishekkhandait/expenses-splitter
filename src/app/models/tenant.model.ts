3eeb74738dbb149a648edcf8e73443dcf52d132c
Abhishek Khandait <khandaitabhishek@gmail.com>
2017-03-28 21:20:06 +0530

    created tenant model

---
+++ src/app/models/tenant.model.ts
@@ -0,0 +1,12 @@
+export class Tenant {
+    name: string;
+    description: string;
+    banner: string;
+}
+
+export const _tenants = [
+    { name: 'Alexis Sanchez', description: 'He is from Chile and plays center forward', banner: '../assets/arsenal.jpg' },
+    { name: 'Mesut Ozil', description: 'He is from Germany and play as attacking midfielder', banner: '../assets/arsenal.jpg' },
+    { name: 'Santi Cazorla', description: 'He is from Spain and plays as midfielder', banner: '../assets/arsenal.jpg'},
+    { name: 'Granit Xhaka', description: 'He is from Switzerland and plays as defensive midfielder', banner: '../assets/arsenal.jpg'}
+];
